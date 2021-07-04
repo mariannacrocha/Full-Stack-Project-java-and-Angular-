96eeba7d0338d486c984e7aba44c22dfab3c85fe
Marianna <marianna.camillo@gmail.com>
2021-07-04 20:35:44 -0300

    Inclusão de GET para as listas

---
+++ todo/src/main/java/com/marianna/todo/resources/TodoResources.java
@@ -0,0 +1,47 @@
+package com.marianna.todo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marianna.todo.domain.Todo;
+import com.marianna.todo.service.TodoService;
+
+
+@RestController
+@RequestMapping(value = "/todos")
+public class TodoResources {
+	
+	@Autowired
+	private TodoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Todo> findById(@PathVariable Integer id) {
+		Todo obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/open")
+	public ResponseEntity<List<Todo>> listOpen() {
+		List<Todo> list = service.findAllOpen();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/close")
+	public ResponseEntity<List<Todo>> listClose() {
+		List<Todo> list = service.findAllClose();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<Todo>> listAll() {
+		List<Todo> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
