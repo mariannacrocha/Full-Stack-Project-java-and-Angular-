034bca683b5b248647853a0b41b6293720c9dc4d
Marianna <marianna.camillo@gmail.com>
2021-07-18 23:35:55 -0300

    inclusao de footer e botoes

---
+++ front/todo/src/app/components/read-all/read-all.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-read-all',
+  templateUrl: './read-all.component.html',
+  styleUrls: ['./read-all.component.css']
+})
+export class ReadAllComponent implements OnInit {
+
+  list: Todo[] = [
+    
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
